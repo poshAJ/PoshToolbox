db357b22885af60eb26b961bfbd8a77e97f93ef0
AJ Raymond <19650958-PoshAJ@users.noreply.gitlab.com>
2025-03-23 09:06:42 -0400

    Update Output by Exception Type (!36)

--- src/Public/Use-ErrorCoalescing.ps1
+++ src/Public/Use-ErrorCoalescing.ps1
@@ -1,7 +1,7 @@
 function Use-ErrorCoalescing {
     # Copyright (c) 2023 Anthony J. Raymond, MIT License (see manifest for details)
     [CmdletBinding()]
-    [Alias("?!")]
+    [Alias('?!')]
     [OutputType([object])]
 
     ## PARAMETERS #############################################################
@@ -24,12 +24,22 @@ function Use-ErrorCoalescing {
     ## PROCESS ################################################################
     process {
         # wrapping in an array to handle $null as input
-        foreach ($Object in @($InputObject)) {
+        foreach ($Object in , $InputObject) {
             try {
                 . $Object
             } catch {
+                $Exception = $_.Exception
+
                 if ($IfError -is [scriptblock]) {
                     . $IfError
+                } elseif ($IfError -is [hashtable]) {
+                    foreach ($Catch in $IfError.GetEnumerator()) {
+                        if (($Condition = $Exception -is $Catch.Name) -and ($Catch.Value -is [scriptblock])) {
+                            . $Catch.Value
+                        } elseif ($Condition) {
+                            Write-Output (, $Catch.Value)
+                        }
+                    }
                 } else {
                     Write-Output (, $IfError)
                 }
