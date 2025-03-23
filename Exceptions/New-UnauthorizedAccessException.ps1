09341fb67f37adab03a70d2e4ba552a2ee454d42
AJ Raymond <19650958-PoshAJ@users.noreply.gitlab.com>
2025-03-23 09:06:36 -0400

    Add Get-FileFolder (!4)

---
+++ Exceptions/New-UnauthorizedAccessException.ps1
@@ -0,0 +1,39 @@
+# Copyright (c) 2023 Anthony J. Raymond, MIT License (see manifest for details)
+
+using namespace System.Management.Automation
+
+function New-UnauthorizedAccessException {
+    [CmdletBinding()]
+    [OutputType([System.Management.Automation.ErrorRecord])]
+
+    ## PARAMETERS #############################################################
+    param(
+        [Parameter(
+            Position = 0,
+            Mandatory
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Message,
+
+        [Parameter()]
+        [switch]
+        $Throw
+    )
+
+    ## PROCESS ################################################################
+    process {
+        $ErrorRecord = [ErrorRecord]::new(
+            [UnauthorizedAccessException]::new($Message),
+            "System.UnauthorizedAccessException",
+            [ErrorCategory]::PermissionDenied,
+            $null
+        )
+
+        if ($Throw) {
+            throw $ErrorRecord
+        }
+
+        Write-Output $ErrorRecord
+    }
+}
