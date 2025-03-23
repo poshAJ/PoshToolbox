0f7eb6c0fc499e315bd59b174072b55d65e6640e
AJ Raymond <19650958-PoshAJ@users.noreply.gitlab.com>
2025-03-23 09:06:37 -0400

    Add Split-File and Join-File (!11)

---
+++ Public/Split-File.ps1
@@ -0,0 +1,118 @@
+# Copyright (c) 2023 Anthony J. Raymond, MIT License (see manifest for details)
+
+using namespace System.IO
+
+function Split-File {
+    [CmdletBinding(SupportsShouldProcess)]
+    [OutputType([object])]
+
+    ## PARAMETERS #############################################################
+    param (
+        [Parameter(
+            Mandatory,
+            Position = 0,
+            ValueFromPipelineByPropertyName,
+            ValueFromPipeline,
+            ParameterSetName = "Path"
+        )]
+        [ValidateScript({
+                if (Test-Path -Path $_ -PathType Leaf) {
+                    return $true
+                }
+                throw "The argument specified must resolve to a valid file path."
+            })]
+        [string[]]
+        $Path,
+
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = "LiteralPath"
+        )]
+        [Alias("PSPath")]
+        [ValidateScript({
+                if (Test-Path -LiteralPath $_ -PathType Leaf) {
+                    return $true
+                }
+                throw "The argument specified must resolve to a valid file path."
+            })]
+        [string[]]
+        $LiteralPath,
+
+        [Parameter()]
+        [ValidateScript({
+                if (Test-Path -LiteralPath $_ -IsValid) {
+                    return $true
+                }
+                throw "The argument specified must resolve to a valid file or folder path."
+            })]
+        [string]
+        $Destination = (Get-Location -PSProvider FileSystem).ProviderPath,
+
+        [Parameter (
+            Mandatory
+        )]
+        [ValidateRange(0, [int32]::MaxValue)]
+        [int32]
+        $Size
+    )
+
+    ## BEGIN ##################################################################
+    begin {
+        $DestinationInfo = [FileInfo] (Resolve-PoshPath -LiteralPath $Destination).ProviderPath
+    }
+
+    ## PROCESS ################################################################
+    process {
+        $ParameterSet = @{ $PSCmdlet.ParameterSetName = $PSBoundParameters[$PSCmdlet.ParameterSetName] }
+        $Process = Resolve-PoshPath @ParameterSet
+
+        foreach ($Object in $Process) {
+            try {
+                if ($Object.Provider.Name -ne "FileSystem") {
+                    New-ArgumentException "The argument specified must resolve to a valid path on the FileSystem provider." -Throw
+                }
+
+                $File = [FileInfo] $Object.ProviderPath
+
+                Write-Verbose ("READ {0}" -f $File)
+                Use-Object ($Reader = [File]::OpenRead($File)) {
+                    $Buffer = [byte[]]::new($Size)
+                    $Count = 1
+
+                    $CalculatedDestination = $DestinationInfo.Extension | ?: { "{0}\{1}" -f $DestinationInfo.Directory.FullName, $File.Name } { "{0}\{1}" -f $DestinationInfo.FullName.TrimEnd("\"), $File.Name }
+
+                    while ($Read = $Reader.Read($Buffer, 0, $Buffer.Length)) {
+                        if ($Read -ne $Buffer.Length) {
+                            [array]::Resize([ref] $Buffer, $Read)
+                        }
+
+                        $SplitFile = "{0}.{1}split" -f $CalculatedDestination, $Count
+                        if ($PSCmdlet.ShouldProcess($SplitFile, "Write Content")) {
+                            if (-not ($Directory = $DestinationInfo.Extension | ?: { $DestinationInfo.Directory } { $DestinationInfo }).Exists) {
+                                $null = [Directory]::CreateDirectory($Directory)
+                            }
+
+                            Write-Verbose ("WRITE {0}" -f $SplitFile)
+                            [File]::WriteAllBytes($SplitFile, $Buffer)
+                        }
+
+                        $Count++
+                    }
+
+                    # sort to fix ChildItem number sorting
+                    Write-Output (Get-ChildItem -Path ("{0}.*split" -f $CalculatedDestination) | Sort-Object -Property @{e = { [int32] [regex]::Match($_.FullName, "\.(\d+)split$").Groups[1].Value } })
+                }
+                ## EXCEPTIONS #################################################
+            } catch [System.Management.Automation.MethodInvocationException] {
+                $PSCmdlet.WriteError(( New-MethodInvocationException -Exception $_.Exception.InnerException ))
+            } catch {
+                $PSCmdlet.WriteError($_)
+            }
+        }
+    }
+
+    ## END ####################################################################
+    end {
+    }
+}
---
+++ docs/Split-File.md
@@ -0,0 +1,141 @@
+﻿---
+external help file: PoshToolbox-help.xml
+Module Name: PoshToolbox
+online version: https://github.com/PoshAJ/PoshToolbox/blob/main/docs/Split-File.md
+schema: 2.0.0
+---
+
+# Split-File
+
+## SYNOPSIS
+Splits large files into several smaller files.
+
+## SYNTAX
+
+### Path
+```
+Split-File -Path <String[]> [-Destination <String>] -Size <Int32> [-WhatIf] [-Confirm] [<CommonParameters>]
+```
+
+### LiteralPath
+```
+Split-File -LiteralPath <String[]> [-Destination <String>] -Size <Int32> [-WhatIf] [-Confirm]
+ [<CommonParameters>]
+```
+
+## DESCRIPTION
+The Split-File cmdlet separates a file into parts of the specified size.
+
+## EXAMPLES
+
+## PARAMETERS
+
+### -Confirm
+Prompts you for confirmation before running the cmdlet.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: cf
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Destination
+Specifies the path to the new location. The default is the current directory.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Size
+Specifies the maximum size in bytes of each split file.
+
+```yaml
+Type: Int32
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -WhatIf
+Shows what would happen if the cmdlet runs.
+The cmdlet is not run.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: wi
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -LiteralPath
+Specifies a path to one or more locations. The value of LiteralPath is used exactly as it's typed.
+
+```yaml
+Type: String[]
+Parameter Sets: LiteralPath
+Aliases: PSPath
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+### -Path
+Specifies a path to one or more locations. Wildcards are accepted.
+
+```yaml
+Type: String[]
+Parameter Sets: Path
+Aliases:
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: True (ByPropertyName, ByValue)
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+### String
+You can pipe a string that contains a file path to this cmdlet.
+
+## OUTPUTS
+
+### PSObject
+Returns a result object for each specified file.
+
+## NOTES
+
+## RELATED LINKS
+
+[None]()
